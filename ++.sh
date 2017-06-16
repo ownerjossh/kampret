a0a1576291d384361aed72517b611cf213441e67
ownerjossh <sutris10mei1996@gmail.com>
2017-06-16 20:09:03 +0700

    Create ++.sh

---
+++ ++.sh
@@ -0,0 +1,16 @@
+# install screenfetch
+cd
+wget 'https://github.com/radhsyn83/debian7/raw/master/screenfetch-dev'
+mv screenfetch-dev /usr/bin/screenfetch-dev
+chmod +x /usr/bin/screenfetch-dev
+echo "clear" >> .profile
+echo "screenfetch-dev" >> .profile
+# install dropbear
+apt-get -y install dropbear
+sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 110 -p 109 -p 2017"/g' /etc/default/dropbear
+echo "/bin/false" >> /etc/shells
+echo "/usr/sbin/nologin" >> /etc/shells
+service ssh restart
+service dropbear restart
