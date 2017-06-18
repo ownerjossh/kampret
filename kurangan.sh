2f71fce11563d3fdd4077b8ebf651bc4873f59c5
ownerjossh <sutris10mei1996@gmail.com>
2017-06-18 09:08:08 +0700

    Update kurangan.sh

--- kurangan.sh
+++ kurangan.sh
@@ -8,7 +8,7 @@ echo "screenfetch-dev" >> .profile
 # install dropbear
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
-sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
+sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=143/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_EXTRA_ARGS=/DROPBEAR_EXTRA_ARGS="-p 443 -p 110 -p 109 -p 2017"/g' /etc/default/dropbear
 echo "/bin/false" >> /etc/shells
 echo "/usr/sbin/nologin" >> /etc/shells
