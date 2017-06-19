36f1ad4d77a2862dabd2f8af29f5027238ec7ca8
ownerjossh <sutris10mei1996@gmail.com>
2017-06-19 07:21:41 +0700

    Create kurangan2.sh

---
+++ kurangan2.sh
@@ -0,0 +1,6 @@
+echo "*/30 * * * * root service dropbear restart" > /etc/cron.d/dropbear
+echo "00 23 * * * root /usr/bin/disable-user-expire" > /etc/cron.d/disable-user-expire
+echo "0 */12 * * * root /sbin/reboot" > /etc/cron.d/reboot
+#echo "@reboot root /usr/bin/user-limit" > /etc/cron.d/user-limit
+#echo "@reboot root /usr/bin/autokill" > /etc/cron.d/autokill
+#sed -i '$ i\screen -AmdS check /root/autokill' /etc/rc.local
